0bc316b9641525edb39a89d187af76d73d469101
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2021-12-10 12:04:33 +0100

    mise a jour du bpm auto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Cardio from './component/cardio';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+const [val,setVal]=useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVal(Math.floor(Math.random() * (200 - 40) + 40));
+
+    }, 1000);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,10 +20,13 @@ function App() {
         <p>
           Affichage des cardiogrammes
         </p>
-        <Cardio/>
+        <div id="bpm"></div>
+        <Cardio data={val}/>
       </header>
     </div>
   );
 }
+
+
 
 export default App;

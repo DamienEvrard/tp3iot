c6836002d032c19f93bd9bb2831c374b07260b9d
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2021-12-10 11:02:19 +0100

    nb affiché avec couleur et message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Cardio from './component/cardio';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Affichage des cardiogrammes
         </p>
+        <Cardio/>
       </header>
     </div>
   );
